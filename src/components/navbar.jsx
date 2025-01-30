3549d5d4f957c17db0b69e810cadeb532128080c
Daravuthwatna Phorn <daravuthwatna@gmail.com>
2025-01-30 23:20:59 +0700

    button scroll

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,40 +5,36 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const items = [
-    {
-      name: "Home",
-    },
-    {
-      name: "About",
-    },
-    {
-      name: "My Work",
-    },
-    {
-      name: "Contact",
-    },
+    { name: "Home" },
+    { name: "About", scroll: "0vh" },
+    { name: "My Work", scroll: "900vh" },
+    { name: "Contact", scroll: "2000vh" },
   ];
+
+  const handleScroll = (vh) => {
+    window.scrollBy({
+      top: window.innerHeight + vh,
+      behavior: "smooth",
+    });
+    setIsOpen(false);
+  };
 
   return (
     <div className="w-full bg-transparent absolute top-0">
       <div className="max-w-screen flex flex-wrap items-center justify-between mx-auto py-5 px-9">
         <div className="flex items-center space-x-3">
           <span className="self-center text-3xl whitespace-nowrap text-blue-900 font-bold">
-            My Protfolio
+            My Portfolio
           </span>
         </div>
         <button
-          data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={isOpen ? "true" : "false"}
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none"
           onClick={() => setIsOpen(!isOpen)}
         >
           <span className="sr-only">Open main menu</span>
           <svg
             className="w-5 h-5"
-            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 17 14"
@@ -55,16 +51,18 @@ const Navbar = () => {
         <div
           className={`${
             isOpen
-              ? "block bg-black/30 h-screen rounded-lg backdrop-blur-md shadow-md md:bg-transparent"
+              ? "block bg-black/30 h-[90vh] rounded-lg backdrop-blur-md shadow-md md:bg-transparent"
               : "hidden"
-          } 
-    w-full md:block md:w-auto`}
-          id="navbar-default"
+          } w-full md:block md:w-auto`}
         >
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             {items.map((item, index) => (
               <li key={index}>
-                <Link className="block py-2 pr-4 pl-3 text-white md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
+                <Link
+                  to="#"
+                  onClick={() => handleScroll(parseFloat(item.scroll))}
+                  className="block py-2 pr-4 pl-3 text-white md:hover:bg-transparent md:hover:text-blue-700"
+                >
                   {item.name}
                 </Link>
               </li>
--- src/page/home/components/hero.jsx
+++ src/page/home/components/hero.jsx
@@ -2,27 +2,37 @@ import React from "react";
 import Profile from "../../../assets/my-img.jpg";
 
 const Hero = () => {
+  const handleScrollDown = () => {
+    window.scrollBy({
+      top: window.innerHeight,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="bg-gradient-to-r from-blue-500 to-lime-500">
       <div className="max-w-screen h-screen flex flex-col sm:flex-row items-center justify-center sm:justify-evenly mx-auto p-4 sm:p-8">
         <div className="text-center sm:text-left mb-8 sm:mb-0">
-          <p className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+          <p className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none bg-gradient-to-r from-blue-900 to-green-600 text-transparent bg-clip-text md:text-5xl xl:text-6xl">
             Phorn Daravuthwatna
           </p>
           <p className="max-w-xl mb-4 text-xl font-extrabold tracking-tight leading-none md:text-lg xl:text-3xl">
             Frontend Web Developer +
           </p>
-          <p className="max-w-xl mb-4 text-4xl sm:text-4xl font-extrabold tracking-tight leading-none bg-gradient-to-r from-blue-700 via-green-700 to-indigo-600 text-transparent bg-clip-text">
+          <p className="max-w-xl mb-4 text-4xl sm:text-4xl font-extrabold tracking-tight leading-none bg-gradient-to-r from-blue-900 via-green-700 to-indigo-600 text-transparent bg-clip-text">
             Full Stack Web Developer
           </p>
-          <button className="bg-white text-blue-500 font-semibold py-2 px-6 rounded-full hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">
+          <button
+            onClick={handleScrollDown}
+            className="bg-white text-blue-500 font-semibold py-2 px-6 rounded-full hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out"
+          >
             Read more
           </button>
         </div>
         <div className="flex justify-center sm:justify-start">
           <img
             src={Profile}
-            className="w-48 h-48 sm:w-64 sm:h-64 md:w-96 md:h-96 rounded-full"
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-96 md:h-96 hidden sm:block rounded-full"
             alt="Profile"
           />
         </div>
